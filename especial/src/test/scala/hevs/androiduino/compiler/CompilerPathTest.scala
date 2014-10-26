c1a14e90bfdf3106b64380a5ba723c0592bb31c3
Christopher Métrailler <christopher.metrailler@epfl.ch>
2014-10-26 22:37:48 +0100

    WIP. Compiler path test. Compile the code and launch a simulation on QEMU. Basic TCP connection works between the Scala Monitor Server and the QEMU client.

---
+++ especial/src/test/scala/hevs/androiduino/compiler/CompilerPathTest.scala
@@ -0,0 +1,73 @@
+package hevs.androiduino.compiler
+
+import java.io.File
+
+import grizzled.slf4j.Logging
+import hevs.androiduino.dsl.components.core.Constant
+import hevs.androiduino.dsl.components.digital.DigitalOutput
+import hevs.androiduino.dsl.components.fundamentals.uint1
+import hevs.androiduino.dsl.generator.{RichFile, CodeGenerator}
+import hevs.androiduino.dsl.utils.OSUtils
+import hevs.especial.simulation.MonitorServer
+
+/**
+ * Path example from a program to the QEMU simulation.
+ */
+object CompilerPathTest extends App with Logging {
+
+  //FIXME: use pipeline to do this. Interrupt if any error occurs
+  class Code {
+    val cst1 = Constant(uint1(v = true))
+    val led1 = DigitalOutput(7)
+    cst1.out --> led1.in
+  }
+
+  val code = new Code
+  val c = CodeGenerator.generateCode("code")
+  val f: RichFile = new File("csrc/src/main.c")
+
+  // Must be in "/usr/bin/"
+  // arm-none-eabi-size
+  // arm-none-eabi-gcc
+  // arm-none-eabi-g++
+  // arm-none-eabi-objcopy
+  info("Compiling...")
+  val makeRes = OSUtils.runWithCodeResult("/usr/bin/make -r -j4 -C csrc/target-qemu/ all")
+  // f.write(c) // TODO: write the generated code to the file
+
+  if (makeRes._1 != 0) {
+    error("Make error !")
+    error(makeRes._2)
+  }
+
+  info("Start QEMU in a new process...")
+
+  // Launch in graphic mode
+  final var runQemu = "./../../stm32/qemu_stm32/arm-softmmu/qemu-system-arm -M stm32-p103 -kernel " +
+    "csrc/target-qemu/csrc.elf -serial null -monitor null"
+  val qemuProcess = OSUtils.runInBackground(runQemu)
+
+  // Monitor
+  val ms = new MonitorServer()
+
+  info("Start monitor. Wait for client...")
+  val m = ms.waitForClient()
+  var s = m.getMessagesSize
+
+  while (s < 10) {
+    s = m.getMessagesSize
+    Thread.sleep(100)
+  }
+
+  // Close the TCP server
+  info("Disconnect server.")
+  m.disconnect()
+  ms.close()
+
+  // Close QEMU
+  info("Kill QEMU.")
+  qemuProcess.destroy()
+
+  info("End")
+  System.exit(0)
+}
