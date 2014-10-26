5dc0a2fc4967fd4e42249a3c79212a3bb0a4e621
Christopher Métrailler <christopher.metrailler@epfl.ch>
2014-10-26 22:06:39 +0100

    Now can run background processes (in a separed Thread). Useful for the QEMU simulation.

--- especial/src/main/scala/hevs/especial/utils/Settings.scala
+++ especial/src/main/scala/hevs/especial/utils/Settings.scala
@@ -1,8 +1,17 @@
 package hevs.especial.utils
 
 /**
- * Created by chm on 26/10/14.
+ * Various settings for the project.
  */
 object Settings {
 
+  /**
+   * Port used by the Scala TCP Monitor Server to communicate with the QEMU client.
+   */
+  final var MONITOR_TCP_CMD_PORT = 14001
+
+  /**
+   * Path to the root folder of QEMU for STM32. Relative to the Scala project path.
+   */
+  final var PATH_QEMU_STM32 = "./../../stm32/qemu_stm32"
 }
