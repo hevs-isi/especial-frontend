42a4db21e64ac23e16b379a9533198f549d56905
Christopher Métrailler <christopher.metrailler@epfl.ch>
2014-11-13 17:49:40 +0100

    QemuLogger trait added. Can be used with any components to add the loggin support in QEMU. The QemuLogger will not work using the real target (better to remove it but no tested for now).

--- especial/src/main/scala/hevs/especial/simulation/QemuLogger.scala
+++ especial/src/main/scala/hevs/especial/simulation/QemuLogger.scala
@@ -1,4 +1,6 @@
-package hevs.especial.utils
+package hevs.especial.simulation
+
+import hevs.especial.dsl.components.hw_implemented
 
 /**
  * Use to trace event of the code running in QEMU.
@@ -11,12 +13,23 @@ object QemuLogger {
   private final val LOGGER_NAME = "QemuLogger"
 
   final val addStartEvent = printEvent("SECTION_START")
-  final val addEndInitEvent = printEvent("SECTION_END_INIT")
+  final val addEndInitEvent = printEvent("SECTION_INIT_END")
   final val addLoopStartEvent = printEvent("SECTION_LOOP_START")
   final val addLoopTickEvent = printEvent("SECTION_LOOP_TICK")
   final val addLoopExitEvent = printEvent("SECTION_LOOP_EXIT")
 
   private def printEvent(event: String) = {
-      s"$LOGGER_NAME::send_event($event);\n" // C code to call the logger
+    s"$LOGGER_NAME::send_event($event);\n" // C code to call the logger
   }
+}
+
+/**
+ * Add to anny component to ability to use the Qemu logger.
+ *
+ * An include file will be append to other.
+ */
+trait QemuLogger extends hw_implemented {
+
+  // Include the necessary to use the logger
+  override def getIncludeCode = super.getIncludeCode :+ "utils/qemulogger.h"
 }
