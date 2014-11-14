ed46cfe797dd333db8be1f1b3ee0d751a3af4e76
Christopher Métrailler <christopher.metrailler@epfl.ch>
2014-11-14 23:22:17 +0100

    Enable the QEMU logger when the Context is created.

--- especial/src/main/scala/hevs/especial/utils/Context.scala
+++ especial/src/main/scala/hevs/especial/utils/Context.scala
@@ -1,17 +1,21 @@
 package hevs.especial.utils
 
 /**
- * Contains the context of the current execution. Use by all blocks of the pipeline.
+ * The context of the current execution. Use by all blocks in the pipeline.
+ * @param progName the name of the executed program
+ * @param loggerEnable enable the QEMU logger to trace events
  */
 class Context(val progName: String, loggerEnable: Boolean = false) {
 
-  /** Qemu logger to trace output information from QEMU. */
-  private var isLoggerEnabled = loggerEnable
-
-  def enableQemuLogger() = isLoggerEnabled = true
-  def isQemuLoggerEnabled = isLoggerEnabled
-
+  /**
+   * Check if the QEMU logger enabled or not.
+   * Used to trace output information from QEMU to the monitor server. Somme additional code is added in the
+   * generated code if it is enabled.
+   */
+  def isQemuLoggerEnabled = loggerEnable
 
   /** Logger used by the pipeline blocks to report any error or information. */
   val log = new Logger
+
+  override def toString = s"Context of '$progName'."
 }
