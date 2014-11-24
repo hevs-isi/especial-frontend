745f06d95c846bd13691b97b0c3be3effe468bc3
Christopher Métrailler <christopher.metrailler@epfl.ch>
2014-11-24 17:40:10 +0100

    Some implicit conversions added.

--- especial/src/main/scala/hevs/especial/dsl/components/ImplicitTypes.scala
+++ especial/src/main/scala/hevs/especial/dsl/components/ImplicitTypes.scala
@@ -3,31 +3,39 @@ package hevs.especial.dsl.components
 import scala.language.implicitConversions
 
 /**
- * Implicit conversions for `fundamentals.CType`.
+ * Implicit conversions for CType <-> Scala.
  */
 object ImplicitTypes {
 
   // From CTypes to Scala
-  implicit def boolToLong(v: bool) = v.asLong
+//  implicit def boolToLong(v: bool) = v.asLong
+//  implicit def uint8ToLong(v: uint8) = v.asLong
+//  implicit def uint16ToLong(v: uint16) = v.asLong
+//  implicit def uint32ToLong(v: uint32) = v.asLong
+//  implicit def int8ToLong(v: int8) = v.asLong
+//  implicit def int16ToLong(v: int16) = v.asLong
+//  implicit def int32ToLong(v: int32) = v.asLong
+//  implicit def floatToFloat(v: float) = v.asFloat
+//  implicit def doubleToFloat(v: double) = v.asFloat
 
-  implicit def uint8ToLong(v: uint8) = v.asLong
+  // Create `CTypes` from `Scala`
 
-  implicit def uint16ToLong(v: uint16) = v.asLong
+  implicit def BooleanToBool(v: Boolean): bool = new bool(v)
 
-  implicit def uint32ToLong(v: uint32) = v.asLong
+  implicit def ByteToInt8(v: Byte): int8 = int8(v)
 
-  implicit def int8ToLong(v: int8) = v.asLong
+  implicit def ShortToInt16(v: Short): int16 = int16(v)
+  implicit def ShortToUint8(v: Short): uint8 = uint8(v)
 
-  implicit def int16ToLong(v: int16) = v.asLong
+  implicit def IntToInt32(v: Int): int32 = int32(v)
+  implicit def IntToUint16(v: Int): uint16 = uint16(v)
 
-  implicit def int32ToLong(v: int32) = v.asLong
+  implicit def LongToUint32(v: Long): uint32 = uint32(v)
 
-  implicit def floatToFloat(v: float) = v.asFloat
+  implicit def FloatToFloat(v: Float): float = float(v)
 
-  implicit def doubleToFloat(v: double) = v.asFloat
+  implicit def DoubleToDouble(v: Double): double = double(v)
 
-  // From Scala to CTypes
-  implicit def BooleanToUint1(v: Boolean): bool = bool(v)
 
   // implicit def intToUInt(x: Int): UInt = UInt(x)
   // implicit def booleanToBool(x: Boolean): Bool = Bool(x)
@@ -37,4 +45,9 @@ object ImplicitTypes {
   // implicit def booleanToInt(x: Boolean): Int = if (x) 1 else 0
 
   // TODO test and add implicit conversions
+
+
+  // Conversions inside CType
+
+  implicit def BoolToUint8(v: bool): uint8 = if (v.asBool) uint8(1) else uint8(0)
 }
