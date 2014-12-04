abe555eeaebca2f7057ce10c139c23aa2da77218
Christopher Métrailler <christopher.metrailler@epfl.ch>
2014-12-04 16:32:16 +0100

    New NOT gate. Invert all type of input: Bool (true/false) and 1/0 for all other. Only available for one input.

--- especial/src/main/scala/hevs/especial/dsl/components/core/logic/Not.scala
+++ especial/src/main/scala/hevs/especial/dsl/components/core/logic/Not.scala
@@ -1,8 +1,59 @@
 package hevs.especial.dsl.components.core.logic
 
-/**
- * Created by chm on 04/12/14.
- */
-class Not {
+import hevs.especial.dsl.components._
+
+import scala.collection.mutable.ListBuffer
+import scala.reflect.runtime.universe._
+
+// FIXME: clean and check. Only for one input ?
+// TODO: test case Sch7
+case class Not[T <: CType : TypeTag]() extends Component with In1 with Out1 with HwImplemented {
+
+  override val description = s"NOT gate"
+
+  private val valName: String = outValName()
+
+  /* I/O management */
+
+  val in = new InputPort[T](this) {
+    override val name = s"in"
+    override val description = "input to invert"
+
+    override def setInputValue(s: String) = {
+      // Invert the value
+      val sInvert = getTypeClass[T] match {
+        case _: `Class`[bool] => s"!$s"
+        case _ => s"""($valName == 0) ? 1 : 0"""
+      }
+      s"$valName = $sInvert"
+    }
+  }
+
+  val out = new OutputPort[T](this) {
+    override val name = s"out"
+    override val description = "inverted value"
+
+    override def getValue: String = valName // input already inverted
+  }
+
+  override def getOutputs= Some(Seq(out))
+
+  override def getInputs = None
+
+
+  /* Code generation */
+
+  override def getGlobalCode = {
+    Some(s"${bool().getType} $valName; // $out")
+  }
+
+  override def getLoopableCode = {
+    // Propagate the output value to connected inputs
+    val in = ComponentManager.findConnections(out)
+    val results: ListBuffer[String] = ListBuffer()
+    for (inPort <- in)
+      results += inPort.setInputValue(out.getValue) + "; // " + inPort
+    Some(results.mkString("\n"))
+  }
 
 }
--- especial/src/test/scala/hevs/especial/genenator/Sch7Code.scala
+++ especial/src/test/scala/hevs/especial/genenator/Sch7Code.scala
@@ -1,8 +1,39 @@
 package hevs.especial.genenator
 
-/**
- * Created by chm on 04/12/14.
- */
-class Sch7 {
+import hevs.especial.dsl.components.core.TickToggle
+import hevs.especial.dsl.components.core.logic.Not
+import hevs.especial.dsl.components.target.stm32stk.DigitalOutput
+import hevs.especial.dsl.components.{Pin, bool}
+
+class Sch7Code extends STM32TestSuite {
+
+  def isQemuLoggerEnabled = true
+
+  def runDslCode(): Unit = {
+    // Inputs
+    val gen1 = TickToggle(bool(true))
+
+    // Logic
+    val not1 = Not[bool]()
+
+    // Output
+    val led1 = DigitalOutput(Pin('C', 12))
+    val led2 = DigitalOutput(Pin('C', 13))
+
+    // Connecting stuff
+    gen1.out --> led1.in
+    gen1.out --> not1.in
+    not1.out --> led2.in
+  }
+
+  runDotGeneratorTest()
+
+  runCodeCheckerTest(hasWarnings = false)
+
+  runCodeOptimizer()
+
+  runDotGeneratorTest(optimizedVersion = true)
+
+  runCodeGenTest()
 
 }
