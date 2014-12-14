0bf1e12db577a153f9b4773444d038740f28d105
Christopher Métrailler <christopher.metrailler@epfl.ch>
2014-12-14 21:03:29 +0100

    Makefile updated for PWMOutput and AnalogInput.

--- especial/csrc/target-qemu/stm32f10x_lib/src/hal/subdir.mk
+++ especial/csrc/target-qemu/stm32f10x_lib/src/hal/subdir.mk
@@ -7,26 +7,39 @@ C_SRCS += \
 /home/chm/git/stm32/stm32f10x_lib/src/hal/helper.c 
 
 CPP_SRCS += \
+/home/chm/git/stm32/stm32f10x_lib/src/hal/analoginput.cpp \
 /home/chm/git/stm32/stm32f10x_lib/src/hal/digitalinput.cpp \
 /home/chm/git/stm32/stm32f10x_lib/src/hal/digitaloutput.cpp \
-/home/chm/git/stm32/stm32f10x_lib/src/hal/gpio.cpp 
+/home/chm/git/stm32/stm32f10x_lib/src/hal/gpio.cpp \
+/home/chm/git/stm32/stm32f10x_lib/src/hal/pwmoutput.cpp 
 
 OBJS += \
+./stm32f10x_lib/src/hal/analoginput.o \
 ./stm32f10x_lib/src/hal/digitalinput.o \
 ./stm32f10x_lib/src/hal/digitaloutput.o \
 ./stm32f10x_lib/src/hal/gpio.o \
-./stm32f10x_lib/src/hal/helper.o 
+./stm32f10x_lib/src/hal/helper.o \
+./stm32f10x_lib/src/hal/pwmoutput.o 
 
 C_DEPS += \
 ./stm32f10x_lib/src/hal/helper.d 
 
 CPP_DEPS += \
+./stm32f10x_lib/src/hal/analoginput.d \
 ./stm32f10x_lib/src/hal/digitalinput.d \
 ./stm32f10x_lib/src/hal/digitaloutput.d \
-./stm32f10x_lib/src/hal/gpio.d 
+./stm32f10x_lib/src/hal/gpio.d \
+./stm32f10x_lib/src/hal/pwmoutput.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
+stm32f10x_lib/src/hal/analoginput.o: /home/chm/git/stm32/stm32f10x_lib/src/hal/analoginput.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C++ Compiler'
+	arm-none-eabi-g++ -mcpu=cortex-m3 -mthumb -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wall -Wextra  -g3 -DSTM32F10X_MD -DHSE_VALUE=80000000 -DUSE_STDPERIPH_DRIVER -DTRACE -I"/home/chm/git/ScalaTest/especial/csrc/include" -I"/home/chm/git/stm32/stm32f10x_lib/include" -I"/home/chm/git/stm32/stm32f10x_lib/include/arm" -I"/home/chm/git/stm32/stm32f10x_lib/include/cmsis" -I"/home/chm/git/stm32/stm32f10x_lib/include/cortexm" -I"/home/chm/git/stm32/stm32f10x_lib/include/diag" -I"/home/chm/git/stm32/stm32f10x_lib/include/freertos" -I"/home/chm/git/stm32/stm32f10x_lib/include/hal" -I"/home/chm/git/stm32/stm32f10x_lib/include/stm32f1" -std=gnu++11 -fabi-version=0 -fno-exceptions -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
 stm32f10x_lib/src/hal/digitalinput.o: /home/chm/git/stm32/stm32f10x_lib/src/hal/digitalinput.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross ARM C++ Compiler'
@@ -52,6 +65,13 @@ stm32f10x_lib/src/hal/helper.o: /home/chm/git/stm32/stm32f10x_lib/src/hal/helper
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross ARM C Compiler'
 	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wall -Wextra  -g3 -DSTM32F10X_MD -DHSE_VALUE=80000000 -DUSE_STDPERIPH_DRIVER -DTRACE -I"/home/chm/git/ScalaTest/especial/csrc/include" -I"/home/chm/git/stm32/stm32f10x_lib/include" -I"/home/chm/git/stm32/stm32f10x_lib/include/arm" -I"/home/chm/git/stm32/stm32f10x_lib/include/cmsis" -I"/home/chm/git/stm32/stm32f10x_lib/include/cortexm" -I"/home/chm/git/stm32/stm32f10x_lib/include/diag" -I"/home/chm/git/stm32/stm32f10x_lib/include/freertos" -I"/home/chm/git/stm32/stm32f10x_lib/include/hal" -I"/home/chm/git/stm32/stm32f10x_lib/include/stm32f1" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+stm32f10x_lib/src/hal/pwmoutput.o: /home/chm/git/stm32/stm32f10x_lib/src/hal/pwmoutput.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C++ Compiler'
+	arm-none-eabi-g++ -mcpu=cortex-m3 -mthumb -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wall -Wextra  -g3 -DSTM32F10X_MD -DHSE_VALUE=80000000 -DUSE_STDPERIPH_DRIVER -DTRACE -I"/home/chm/git/ScalaTest/especial/csrc/include" -I"/home/chm/git/stm32/stm32f10x_lib/include" -I"/home/chm/git/stm32/stm32f10x_lib/include/arm" -I"/home/chm/git/stm32/stm32f10x_lib/include/cmsis" -I"/home/chm/git/stm32/stm32f10x_lib/include/cortexm" -I"/home/chm/git/stm32/stm32f10x_lib/include/diag" -I"/home/chm/git/stm32/stm32f10x_lib/include/freertos" -I"/home/chm/git/stm32/stm32f10x_lib/include/hal" -I"/home/chm/git/stm32/stm32f10x_lib/include/stm32f1" -std=gnu++11 -fabi-version=0 -fno-exceptions -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
