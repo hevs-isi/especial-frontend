43c4d99f828126e869ddf33a55b3651304d8918a
Christopher Métrailler <christopher.metrailler@epfl.ch>
2014-12-14 14:15:08 +0100

    PWM output available from the Scala side. Available only for led3 and led4 of the extension board. Use the timer4. Not available for all GPIO for now...

---
+++ especial/src/main/scala/hevs/especial/dsl/components/target/stm32stk/PwmOutput.scala
@@ -0,0 +1,80 @@
+package hevs.especial.dsl.components.target.stm32stk
+
+import hevs.especial.dsl.components._
+
+/**
+ * Create a Pulse-width modulation (PWM) output for a specific pin.
+ *
+ * Initialize the output and set it to `OFF` by default.
+ *
+ * @param pin the pin of the GPIO (port and pin number)
+ */
+class PwmOutput private(private val pin: Pin) extends Gpio(pin) with In1 with HwImplemented {
+
+  override val description = s"PWM output\\non $pin"
+
+  private val valName = outValName()
+  private var initialized: Boolean = false
+
+  /* I/O management */
+
+  /**
+   * The `uint16` period value to write to this PWM output.
+   */
+  override val in = new InputPort[uint16](this) {
+    override val name = s"in"
+    override val description = "PWM output value"
+
+    override def setInputValue(s: String): String = s"$valName.setPeriod($s)"
+  }
+
+  override def getOutputs = None
+
+  override def getInputs = Some(Seq(in))
+
+
+  /* Code generation */
+
+  override def getGlobalCode = Some(s"PwmOutput $valName($pinName); // $in")
+
+  override def getInitCode = {
+    // Initialize the output in the `initOutputs` function. Do it only once !
+    if (!initialized) {
+      initialized = true // Init code called once only
+      Some(s"$valName.initialize(); // Init of $this")
+    }
+    else {
+      // Output already initialized in the `initOutputs` function.
+      // Nothing to do in the `init` function.
+      None
+    }
+  }
+
+  override def getIncludeCode = Seq("pwmoutput.h")
+}
+
+/**
+ * Create a PWM output for a specific pin.
+ *
+ * The output pin should be unique. If is not possible to create two PWM output for the same pin.
+ * The [[PwmOutput]] constructor is private and a [[PwmOutput]] must be created using this companion object to
+ * be sure than only one output exist for this pin.
+ */
+object PwmOutput {
+
+  /**
+   * Create a PWM output input for a specific pin.
+   *
+   * @param pin the pin of the GPIO (port and pin number)
+   * @return the PWM output or the existing one if already in the graph
+   */
+  def apply(pin: Pin): PwmOutput = {
+    val tmpCmp = new PwmOutput(pin)
+    // Check of the output already exist in the graph.
+    // If yes, return the existing component. If no, return a new component.
+    val isAdded = ComponentManager.addComponent(tmpCmp)
+
+    // Return the existing component if defined or the new added component
+    isAdded.getOrElse(tmpCmp)
+  }
+}
