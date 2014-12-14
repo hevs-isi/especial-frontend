c83d04a4d96e06617d5ce7eb9b2dc4ec67cda824
Christopher Métrailler <christopher.metrailler@epfl.ch>
2014-12-14 14:14:01 +0100

    Analog input available from the Scala side. Use the A/D to convert an analog value to an uint16 digital value. The A/D channel must be specified.

---
+++ especial/src/main/scala/hevs/especial/dsl/components/target/stm32stk/AnalogInput.scala
@@ -0,0 +1,91 @@
+package hevs.especial.dsl.components.target.stm32stk
+
+import hevs.especial.dsl.components._
+
+/**
+ * Create an analog for a specific pin.
+ *
+ * Use an A/D converter to read an analog input value. The channel used for the conversion must be specified.
+ * The converted value is saved in a global variable in [[uint16]]] format.
+ *
+ * @param pin the pin of the GPIO (port and pin number)
+ * @param channel the A/D channel used for the conversion
+ */
+class AnalogInput private(private val pin: Pin, private val channel: Int) extends Gpio(pin)
+  with Out1 with HwImplemented {
+
+  override val description = s"analog input\\non $pin"
+
+  /**
+   * The analog value converted to a `uint16` digital value.
+   */
+  override val out = new OutputPort[uint16](this) {
+    override val name = s"out"
+    override val description = "analog input value"
+
+    override def getValue: String = s"$fctName();"
+  }
+  private val valName = inValName()
+
+  /* I/O management */
+  private val fctName = s"getlAnalogInput${pin.port}${pin.pinNumber}"
+
+  override def getOutputs = Some(Seq(out))
+
+  override def getInputs = None
+
+  /* Code generation */
+
+  override def getGlobalCode = Some(s"AnalogInput $valName($pinName, $channel); // $out")
+
+  override def getInitCode = Some(s"$valName.initialize(); // Init of $this")
+
+  override def getLoopableCode = Some(s"$fctName();")
+
+  override def getFunctionsDefinitions = {
+    // Add a function to get the cached value of this input.
+    val res = new StringBuilder
+
+    // 1) Store the input value in a local variable
+    res ++= s"void $fctName() {\n"
+    res ++= "// Start an A/D conversion and wait for the result\n"
+    res ++= s"${uint16().getType} val = $valName.read();\n"
+
+    // 2) Propagate this value to all connected components
+    val in = ComponentManager.findConnections(out)
+    for (inPort ← in)
+      res ++= inPort.setInputValue("val") + s"; //$inPort\n"
+
+    res ++= "}"
+    Some(res.result())
+  }
+
+  override def getIncludeCode = Seq("analoginput.h")
+}
+
+/**
+ * Create a analog input for a specific pin.
+ *
+ * The input pin should be unique. If is not possible to create two analog input for the same pin.
+ * The [[AnalogInput]] constructor is private and a [[AnalogInput]] must be created using this companion object to
+ * be sure than only one output exist for this pin.
+ */
+object AnalogInput {
+
+  /**
+   * Create an analog input for a specific pin.
+   *
+   * @param pin the pin of the GPIO (port and pin number)
+   * @param channel the A/D channel used for the conversion
+   * @return the analog input or the existing one if already in the graph
+   */
+  def apply(pin: Pin, channel: Int): AnalogInput = {
+    val tmpCmp = new AnalogInput(pin, channel)
+    // Check of the output already exist in the graph.
+    // If yes, return the existing component. If no, return a new component.
+    val isAdded = ComponentManager.addComponent(tmpCmp)
+
+    // Return the existing component if defined or the new added component
+    isAdded.getOrElse(tmpCmp)
+  }
+}
