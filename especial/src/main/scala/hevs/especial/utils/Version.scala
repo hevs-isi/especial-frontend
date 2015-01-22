c3b5226588999a11ab2d0059a05b4db4bcd0a4dd
Christopher Métrailler <christopher.metrailler@epfl.ch>
2015-01-22 09:02:15 +0100

    Bump version B3.

--- especial/src/main/scala/hevs/especial/utils/Version.scala
+++ especial/src/main/scala/hevs/especial/utils/Version.scala
@@ -2,12 +2,15 @@ package hevs.especial.utils
 
 /**
  * Project name and version.
+ * Name and version added in the header of all generated files.
+ *
+ * @author Christopher Metrailler (mei@hevs.ch)
  */
 object Version {
 
   private val name = "ESPecIaL"
 
-  val major = "B2"
+  val major = "B3"
   val minor = 0
 
   override def toString = s"$name version $getVersion"
